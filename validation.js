39c3fde0298703ffda75f12972dc9a3929eb22df
loloperz <loloperez1995@gmail.com>
2023-11-30 15:41:08 +0100

    iniciojs9

--- validation.js
+++ validation.js
@@ -22,21 +22,47 @@
 // })()
 export default (() => {
 
-    const formInput = document.querySelector(".form");
-    console.log(formInput)
-    input?.addEventListener("input", () => {
-        const validate = input.value;
+    const form = document.querySelector(".form");
+    
+    form?.addEventListener("input", (event) => {
 
-        const minLength = parseInt(input.dataset.minlength);
-        
-        if (validate.length < minLength) {
-           
-            input.classList.add("invalid");
+        if(event.target.closest('[data-minlength')){
+            const input = event.target.closest('[data-minlength')
+            
+            if (input.value.length < input.dataset.minlength) {
+            
+                input.classList.add("invalid");
+            }
+            else {
+                input.classList.remove("invalid");
+            }
         }
-        else {
-            input.classList.remove("invalid");
 
+        if(event.target.closest('[data-onlyletters')){
+            const onlyletters= event.target.closest('[data-onlyletters]');
+     
+            const letters=  /^[A-Za-z\s]+$/;
+
+            if (!event.target.value.match(letters)){
+                onlyletters.classList.add("invalid");
+            }
+            else{
+                onlyletters.classList.remove("invalid");
+            }
         }
+        if(event.target.closest('[data-email')){
+            const onlyEmail= event.target.closest('[data-email]');
+     
+            const email= /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+            if (!event.target.value.match(email)){
+                onlyEmail.classList.add("invalid");
+            }
+            else{
+                onlyEmail.classList.remove("invalid");
+            }
+        }
+      
     })
 
 
