24277096930bb292b8d77266430d207bdda5a0d2
loloperz <loloperez1995@gmail.com>
2023-12-01 15:49:04 +0100

    estructura-js13

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-import './menu.js';
+import './menu-modulo.js';
 import "./filter.js";
 import "./table.js";
 import "./tabs.js"
 import "./modal-destroy.js"
 import "./validation.js"
 import "./title.js"
+import "./menu.js"
---
+++ menu-modulo.js
@@ -0,0 +1,10 @@
+export default (() => {
+
+   const menuButton = document.querySelector('.menu-button');
+   const menu = document.querySelector(".menu");
+
+   menuButton?.addEventListener("click", () => {
+      menu.classList.toggle("active");
+   });
+})();
+ 
