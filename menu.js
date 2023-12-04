d10f2c20ad1dfa7bb2a607dd3c6195d85fb56531
loloperz <loloperez1995@gmail.com>
2023-12-04 13:31:29 +0100

    menu--web-components

--- menu.js
+++ menu.js
@@ -1,66 +1,73 @@
 class Menu extends HTMLElement {
 
-    constructor() {
-        super()
-        this.shadow = this.attachShadow({ mode: 'open' })
-        this.menu = this.getAttribute('menu')
-    }
+  constructor() {
+    super()
+    this.shadow = this.attachShadow({ mode: 'open' })
+  }
 
-    connectedCallback() {
-        this.render()
-    }
+  connectedCallback() {
+    this.render()
+  }
 
-    render() {
-        this.shadow.innerHTML =
+  render() {
+    this.shadow.innerHTML =
       /*html*/`
         <style>
-            .menu-button {
-  position: relative;
-  z-index: 3001;
-}
 
-.menu-button button svg {
-  width: 3rem;
-}
+          button{
+            background: none;
+            border: none;
+            cursor: pointer;
+          }
+          
+          .menu-button {
+            position: relative;
+            z-index: 3001;
+          }
 
-.menu-button button svg path {
-  fill: hsl(0, 0%, 100%);
-}
+          .menu-button button svg {
+            width: 3rem;
+          }
 
-.menu {
-  background-color: hsl(19, 63%, 45%);
-  position: fixed;
-  top: -100vh;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  transition: top 0.3s;
-  z-index: 3000;
-}
+          .menu-button button svg path {
+            fill: hsl(0, 0%, 100%);
+          }
 
-.menu.active {
-  top: 0;
-}
+          .menu {
+            background-color: hsl(19, 63%, 45%);
+            position: fixed;
+            top: -100vh;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            transition: top 0.3s;
+            z-index: 3000;
+          }
+
+          .menu.active {
+            top: 0;
+          }
         </style>
-  
-  
-<div class="menu-button">
-  <button>
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-          <title>menu</title>
-          <path d="M3,6H21V8H3V6M3,11H21V13H3V11M3,16H21V18H3V16Z" />
-      </svg>
-  </button>
-</div>
-<div class="menu"></div
+          
+          
+        <div class="menu-button">
+          <button>
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                  <title>menu</title>
+                  <path d="M3,6H21V8H3V6M3,11H21V13H3V11M3,16H21V18H3V16Z" />
+              </svg>
+          </button>
+        </div>
+        <div class="menu"></div>
       `
-    }
-    const menuButton = document.querySelector('.menu-button');
-    const menu = document.querySelector(".menu");
- 
-    menuButton?.addEventListener("click", () => {
-       menu.classList.toggle("active");
-    });
+
+      const menu = this.shadow.querySelector('.menu');
+      const menuButton = this.shadow.querySelector('.menu-button');
+
+      menuButton.addEventListener("click", () => {
+        menu.classList.toggle("active");
+     })
+  }
     
 }
 
